02d6c176355a8f1d2811efc964d7cf8eaf48c19b
fausto <fausto.parada12@gmail.com>
2024-04-05 20:55:06 -0300

    usando json

--- fausto/tp1/fn.js
+++ fausto/tp1/fn.js
@@ -1,24 +1,22 @@
-/*$array = ['pera','manzana','frutilla','banana','naranja']
+/*/*$array = ['pera','manzana','frutilla','banana','naranja']
 
 /*mostrando un elemento del array directamente con la posicion*/
-/*console.log($array[1])
+console.log($array[1])
 
 /*recorriendo el array y mostrandolo completo(primera forma)*/
 /*for(i=0; i<$array.length; i++){
     console.log($array[i])
 } 
- */
-
 
 let frutas = ['pera','manzana','frutilla','banana','naranja']
 /*recorriendo array con foreach (segunda forma) */
-frutas.forEach(fruta => {
+/*frutas.forEach(fruta => {
     console.log(fruta);
 });
 
-/*  recorriendo array (tercera forma) */
-for(const fruta of frutas){
+ /* recorriendo array (tercera forma) */
+/*for(const fruta of frutas){
     if (fruta=='manzana') {
     console.log(fruta)
     }
-}
+} 
